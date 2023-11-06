1a3530aef547bfe9cf62ee0f7d9ba57ddc706795
MakaevT <brunsmayer@bk.ru>
2023-11-06 21:56:23 +0300

    done

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+app.use(express.json())
+app.use(require('./routes/drinks.route'))
+
+
+mongoose.connect("mongodb+srv://brunsmayer:brunsmayer@cluster0.26hjj67.mongodb.net/", {
+  }).then(() => console.log('Успешно соединились с сервером MongoDB'))
+  .catch(() => console.log('Ошибка при соединении с сервером MongoDB'))
+
+app.listen(3000, () => console.log('server has been started'))
